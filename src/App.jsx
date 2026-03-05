3502728ad0112f42a4ff294aea522a08be55bcec
Aneesa Inayat <aneesainayat22@gmail.com>
2026-03-05 12:09:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello React App!</h1>
-    </div>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
